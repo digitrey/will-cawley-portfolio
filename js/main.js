1983922ed7754741c1568beb574ea564d73c10be
digitrey <38080262+digitrey@users.noreply.github.com>
2023-06-27 20:47:02 -0500

    6/27 Iteration 1

---
+++ js/main.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var options;
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(modals, options);
+    
+    //Initializing modals
+    var modalOptions = {
+        opacity: 0.8,
+        preventScrolling: false
+    };
+    var modals = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(modals, modalOptions);
+});
+
+//Integer
+let variableNumber = 2;
+
+//String
+let color = "red";
+
+//Change anything to red background
+function changeColor(elementId) {
+    document.getElementById(elementId).style.backgroundColor = color;
+}
+
+//Create a new button button
+function createButton() {
+    let newButton = document.createElement('a');
+    newButton.className = "btn";
+    newButton.innerHTML = "New Button!"
+    document.getElementById("hero").appendChild(newButton);
+}
+
+//Dark Mode
+function toggleDark() {
+    document.body.classList.toggle('dark');
+}
+
+function toggleNav() {
+    document.getElementById('navWrapper').classList.toggle('active');
+    document.getElementById('navTrigger').classList.toggle('active');
+}
